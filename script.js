2df12aa12714d6e2d6fc5eaa4fa6f4ca38509167
Ajay199707 <129852480+Ajay199707@users.noreply.github.com>
2025-04-16 06:20:08 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const menuToggle  =document.getElementById('menuToggle')
-const navMenu =document.getElementById('navMenu')
-
-menuToggle.addEventListener('click', ()=>{
-    navMenu.style.display = navMenu.style.display === 'flex'? 'none': 'flex';
-
-});
