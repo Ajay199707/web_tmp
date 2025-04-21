00adb3bf6162af7a64545fd0d20eb90a453db6c6
AJAY <129852480+Ajay199707@users.noreply.github.com>
2025-04-21 07:42:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
-const menuToggle  =document.getElementById('navToggle')
-const navMenu =document.getElementById('navMenu')
+const menuToggle  = document.getElementById('navToggle');
+const navMenu = document.getElementById('navMenu');
 
-menuToggle.addEventListener('click', ()=>{
-    menuToggle.classList.toggle('glow');
-    navMenu.classList.toggle('show');
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('glow');
+  navMenu.classList.toggle('show');
 });
+
+
+    let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("mySlidess");
+  const dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].classList.add("active");
+}
+
+// Auto slideshow (optional)
+setInterval(() => {
+  plusSlides(1);
+}, 3000);
+
+
+
 
